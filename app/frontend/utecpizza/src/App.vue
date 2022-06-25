c23ed417228f5b41152a1c32413d5836198c6955
MariaSurco <maria.surco@utec.edu.pe>
2022-06-25 13:12:41 -0500

    actualizacion de archivos vue

--- app/frontend/utecpizza/src/App.vue
+++ app/frontend/utecpizza/src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">hola</router-link> |
+    <router-link to="/pizza">Pizza 🍕 </router-link> |
+    <router-link to="/entradas">Entradas 🥟 </router-link> |
+    <router-link to="/gaseosas">Gaseosas 🥤</router-link> |
+    <router-link to="/carrito">Carrito 🛒 </router-link> |
+    <router-link to="/logout">Log-out</router-link> |
   </nav>
   <router-view />
 </template>
@@ -11,7 +15,7 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
 }
 
--- app/frontend/utecpizza/src/router/index.js
+++ app/frontend/utecpizza/src/router/index.js
@@ -1,20 +1,41 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import pizza from "../views/pizza.vue";
+import entradas from "../views/entradas.vue";
+import gaseosas from "../views/gaseosas.vue";
+import carrito from "../views/carrito.vue";
+import logout from "../views/logout";
 
 const routes = [
+  {
+    path: "/logout",
+    name: "Logout",
+    component: logout,
+  },
+  {
+    path: "/carrito",
+    name: "Carrito",
+    component: carrito,
+  },
+  {
+    path: "/gaseosas",
+    name: "Gaseosas",
+    component: gaseosas,
+  },
+  {
+    path: "/entradas",
+    name: "Entradas",
+    component: entradas,
+  },
+  {
+    path: "/pizza",
+    name: "Pizza",
+    component: pizza,
+  },
   {
     path: "/",
     name: "home",
     component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
 ];
 
