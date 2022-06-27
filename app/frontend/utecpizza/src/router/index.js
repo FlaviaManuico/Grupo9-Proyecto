0db0775aa515bf7a757ab6b763339f4aa443ee8f
Flavia Manuico <flavia.manuico@utec.edu.pe>
2022-06-26 19:48:12 -0500

    Modificacion del archivo index

--- app/frontend/utecpizza/src/router/index.js
+++ app/frontend/utecpizza/src/router/index.js
@@ -1,17 +1,10 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 import pizza from "../views/pizza.vue";
 import entradas from "../views/entradas.vue";
 import gaseosas from "../views/gaseosas.vue";
 import carrito from "../views/carrito.vue";
-import logout from "../views/logout";
 
 const routes = [
-  {
-    path: "/logout",
-    name: "Logout",
-    component: logout,
-  },
   {
     path: "/carrito",
     name: "Carrito",
@@ -34,8 +27,8 @@ const routes = [
   },
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "Pizza",
+    component: pizza,
   },
 ];
 
