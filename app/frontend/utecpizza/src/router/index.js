c831cc830c0971c2879a8680d33444d328e758d2
Alvaro <alvaro.riojas@utec.edu.pe>
2022-06-27 19:25:50 -0500

    webhash -> html5

--- app/frontend/utecpizza/src/router/index.js
+++ app/frontend/utecpizza/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import pizza from "../views/pizza.vue";
 import entradas from "../views/entradas.vue";
 import gaseosas from "../views/gaseosas.vue";
@@ -33,7 +33,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
