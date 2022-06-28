9d69a8ead76ca0ab96503a4d9065d1b39189dc58
Alvaro <alvaro.riojas@utec.edu.pe>
2022-06-27 22:10:07 -0500

    inedx.js pequeño cambio

--- app/frontend/utecpizza/src/router/index.js
+++ app/frontend/utecpizza/src/router/index.js
@@ -1,34 +1,41 @@
 import { createRouter, createWebHistory } from "vue-router";
-import pizza from "../views/pizza.vue";
-import entradas from "../views/entradas.vue";
-import gaseosas from "../views/gaseosas.vue";
-import carrito from "../views/carrito.vue";
 
 const routes = [
   {
+    path: "/ingresar",
+    name: "Ingresar",
+    component: () =>
+      import(/* webpackChunkName: "ingresar" */ "../views/ingresar.vue"),
+  },
+  {
     path: "/carrito",
     name: "Carrito",
-    component: carrito,
+    component: () =>
+      import(/* webpackChunkName: "carrito" */ "../views/carrito.vue"),
   },
   {
     path: "/gaseosas",
     name: "Gaseosas",
-    component: gaseosas,
+    component: () =>
+      import(/* webpackChunkName: "gaseosas" */ "../views/gaseosas.vue"),
   },
   {
     path: "/entradas",
     name: "Entradas",
-    component: entradas,
+    component: () =>
+      import(/* webpackChunkName: "entradas" */ "../views/entradas.vue"),
   },
   {
     path: "/pizza",
     name: "Pizza",
-    component: pizza,
+    component: () =>
+      import(/* webpackChunkName: "pizza" */ "../views/pizza.vue"),
   },
   {
     path: "/",
     name: "Pizza",
-    component: pizza,
+    component: () =>
+      import(/* webpackChunkName: "pizza" */ "../views/pizza.vue"),
   },
 ];
 
