9a0dec3d239da1c69e3a1f58ed47d93b0fbe2029
Flavia Manuico <flavia.manuico@utec.edu.pe>
2022-07-02 18:27:35 -0500

    Se agrega el def create_app y app.after_request

--- app/backend/server/__init__.py
+++ app/backend/server/__init__.py
@@ -9,3 +9,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager,UserMixin, current_user, login_user, logout_user, login_required
 from flask_cors import CORS, cross_origin
 from models import setup_db, usuario, producto, pedido, detallesPedido
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app, origins =['http://127.0.0.1:5000', 'http://localhost:8080'])
+
+    @app.after_request
+    def after_resquest(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorizations, true')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
+        return response
+        
+    return app
