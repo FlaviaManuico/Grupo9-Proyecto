afd84bdce32d2c830780f14cb8bcd5ce5cff4137
MariaSurco <maria.surco@utec.edu.pe>
2022-07-01 22:05:14 -0500

    actualizacion de models.py

--- app/backend/models.py
+++ app/backend/models.py
@@ -51,3 +51,59 @@ class usuario(UserMixin,db.Model):
 
     def __repr__(self):
         return f'Usuario: id={self.id}, usuario={self.usuario}, contrasena={self.contrasena}, nombre={self.nombre}, apellido={self.apellido}, email={self.email}, direccion={self.direccion}, telefono={self.telefono}'
+
+
+class producto(db.Model):
+     __tablename__ = 'productos'
+     id = db.Column(db.Integer, primary_key=True)
+     comida = db.Column(db.String(), nullable=False)
+     precio = db.Column(db.Float(), nullable=False)
+     detalles = db.relationship('detallesPedido', backref='detalles0', lazy=True)
+
+
+    
+     def __repr__(self):
+        return f'Producto: id={self.id} comida={self.comida}, precio={self.precio}'
+
+class pedido(db.Model):
+     __tablename__ = 'pedidos'
+     id = db.Column(db.Integer(), primary_key=True)
+     user_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'), nullable=False)
+     fechaPedido = db.Column(db.DateTime, default=datetime.now())
+     detalle = db.relationship('detallesPedido', backref='detalles', lazy=True)
+
+     def __repr__(self):
+         return f'Pedido: id={self.id} fechaPedido={self.fechaPedido}'
+
+class detallesPedido(db.Model):
+    __tablename__ = 'detalles_pedidos'
+    id = db.Column(db.Integer, primary_key=True)
+    pedido_id = db.Column(db.Integer, db.ForeignKey('pedidos.id'), nullable=False)
+    producto_id = db.Column(db.Integer, db.ForeignKey('productos.id'), nullable=False)
+    cantidad= db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'Carrito: id={self.id}, cantidad={self.cantidad}'
+
+# class carrito_compra(db.Model):
+#     __tablename__ = 'carrito'
+#     id = db.Column(db.Integer, primary_key=True)
+#     comida = db.Column(db.String(), nullable=False)
+#     precio = db.Column(db.Float(), nullable=False)
+
+#     def __repr__(self):
+#         return f'Carrito: id={self.id} comida={self.comida}, precio={self.precio}'
+
+# PRODUCTOS
+# Entradas
+pAjo2 = producto(comida='Pan al Ajo (x2)', precio=4.00)
+pAjo4 = producto(comida='Pan al Ajo (x4)', precio=8.00)
+pQueso2 = producto(comida='Palitos de Queso (x2)', precio=5.50)
+pQueso4 = producto(comida='Palitos de Queso (x4)', precio=11.00)
+
+# Pizzas
+PHf = producto(comida='Pizza Hawaiana Familiar', precio=32.90)
+PHg = producto(comida='Pizza Hawaiana Grande', precio=22.90)
+PHm = producto(comida='Pizza Hawaiana Mediana', precio=16.90)
+PHp = producto(comida='Pizza Hawaiana Personal', precio=10.00)
+PAf = producto(comida='Pizza Americana Familiar', precio=29.90)
